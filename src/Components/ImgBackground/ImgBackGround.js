ea29b234915210a601f7eec8d170b36ec1569e2f
EdebaliErman <edebalierman@gmail.com>
2023-11-28 21:50:51 +0300

    arka plan için komponent oluşturdum

--- src/Components/ImgBackground/ImgBackGround.js
+++ src/Components/ImgBackground/ImgBackGround.js
@@ -4,8 +4,8 @@ function ImgBackGround({desktop,mobil}) {
     const url= process.env.PUBLIC_URL+"./assets/"
   return (
     <>
-      <img className='w-full sm:hidden md:hidden' src={url+desktop} alt='img'/>
-      <img className='w-full hidden sm:flex md:flex' src={url+mobil} alt='img'/>
+      <img className='w-full sm:hidden md:hidden'  src={url+desktop} alt='img'/>
+      <img className='w-full hidden sm:flex md:flex'  src={url+mobil} alt='img'/>
     </>
   )
 }
