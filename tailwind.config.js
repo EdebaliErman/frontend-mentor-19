428454402065300437879a30405ffbaed26ea5ce
EdebaliErman <edebalierman@gmail.com>
2023-11-28 21:17:39 +0300

    Style add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Pink: "hsl(322, 100%, 66%)",
+        LightPink: "hsl(321, 100%, 78%)",
+        LightRed: "hsl(0, 100%, 63%)",
+        VeryDarkCyan: "hsl(192, 100%, 9%)",
+        VeryPaleBlue: "hsl(207, 100%, 98%)",
+      },
+      fontFamily:{
+        Open:['Open Sans'," sans-serif"],
+        popins:['Poppins'," sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
