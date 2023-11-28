e1e5badf2ae0f5447f1f5cc0697b15c317156404
EdebaliErman <edebalierman@gmail.com>
2023-11-28 23:30:58 +0300

    About Tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './Components/About/About';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About/>
     </div>
   );
 }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,56 @@
 import React from 'react'
+import ImgBackGround from '../ImgBackground/ImgBackGround'
+import "../../Style/About.css"
+
+const url = process.env.PUBLIC_URL + "./assets/"
 
 function About() {
   return (
-    <div>
-      
+    <div className='About'>
+      <ImgBackGround
+        mobil={"bg-section-top-mobile-1.svg"}
+        desktop={"bg-section-top-desktop-1.svg"} />
+      <Card
+        img={"illustration-grow-together.svg"}
+        title={"Grow Together"}
+        text={"Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form."}
+        style={"Card"}
+      />
+      <ImgBackGround
+        mobil={"bg-section-bottom-mobile-1.svg"}
+        desktop={"bg-section-bottom-desktop-1.svg"} />
+      <Card
+        img={"illustration-flowing-conversation.svg"}
+        title={"Flowing Conversations"}
+        text={"You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow."}
+        style={"CardR"}
+      />
+     <ImgBackGround
+        mobil={"bg-section-top-mobile-2.svg"}
+        desktop={"bg-section-top-desktop-2.svg"} />
+      <Card
+        img={"illustration-your-users.svg"}
+        title={"You Users"}
+        text={"It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately."}
+        style={"Card"}
+      />
+      <ImgBackGround
+        mobil={"bg-section-bottom-mobile-2.svg"}
+        desktop={"bg-section-bottom-desktop-2.svg"} />
     </div>
   )
 }
+
+const Card = ({ img, title, text, style }) => {
+  return <div className={style}>
+    <div>
+      <h1>{title}</h1>
+      <h5>{text}</h5>
+    </div>
+    <img src={url + img} alt='img' />
+  </div>
+}
+
+
 
 export default About
