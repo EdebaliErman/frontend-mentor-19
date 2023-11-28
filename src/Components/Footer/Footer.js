972c28a3215865fd087e7d684f526d98480e10af
EdebaliErman <edebalierman@gmail.com>
2023-11-29 02:47:11 +0300

    Footer Finished

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ const url = process.env.PUBLIC_URL + "./assets/"
 
 function Footer() {
   return (
-    <div>
+    <div className='Footer'>
       <FooterHeader />
       <ImgBackGround mobil={"bg-footer-top-mobile.svg"}
         desktop={"bg-footer-top-desktop.svg"}
@@ -33,10 +33,10 @@ const FooterBody = () => {
 
 
 const HandleCard = () => {
-  return <div>
-    <img src={url + "logo.svg"} alt='logo' />
+  return <div className='HandleCard'>
+    <img className='w-1/2 md:w-56 md:mt-12 ' src={url + "footerlogo.svg"} alt='logo' />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.</p>
-    <ul>
+    <ul className='flex-col'>
       <li>
         <img src={url + "icon-phone.svg"} alt='phone' />
         <h5>+1-543-123-4567</h5>
@@ -55,13 +55,15 @@ const HandleCard = () => {
 }
 
 const Newsletter = () => {
-  return <div>
+  return <div className='Newsletter'>
     <h1>Newsletter</h1>
     <p> To recieve tips on how to grow your community, sign up to our weekly newsletter. We'll never send you spam or pass on your email address</p>
-    <div>
-      <input type='mail' />
-      <button>Subscribe</button>
+    <form> <div className='subscribe'>
+
+      <input type='email' />
+      <button type='submit'>Subscribe</button>
     </div>
+    </form>
   </div>
 }
 export default Footer
